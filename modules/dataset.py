86e67414b97cd9b9df3f37bc6a685342604c1a2b
filippo <filipposchiazza97@gmail.com>
2024-02-19 15:10:07 +0100

    Add subpatch sampling in dataset.py

--- modules/dataset.py
+++ modules/dataset.py
@@ -6,7 +6,11 @@ from torchvision.io import read_image
 
 class ImageDataset(data.Dataset):
 
-    def __init__(self, img_dir, transform=None, fraction=1.0):
+    def __init__(self, 
+                 img_dir, 
+                 transform=None, 
+                 fraction=1.0):
+        super(ImageDataset, self).__init__()
         self.img_dir = img_dir
         self.transform = transform
         self.fraction = fraction
@@ -26,15 +30,51 @@ class ImageDataset(data.Dataset):
             img = self.transform(img)
         return img
     
-    
+
+
+class SubpatchImageDataset(ImageDataset):
+
+    def __init__(self, 
+                 img_dir, 
+                 img_size, 
+                 patch_size,
+                 seed=1,
+                 transform=None, 
+                 fraction=1.0):
+        super(SubpatchImageDataset, self).__init__(img_dir, transform, fraction)
+        assert img_size > patch_size, "img_size must be greater than patch_size"
+        self.img_size = img_size
+        self.patch_size = patch_size
+        torch.manual_seed(seed)
+
+    def __getitem__(self, idx):
+        img = super().__getitem__(idx)  # (3, img_size, img_size)
+        # extract random patch
+        x = torch.randint(0, self.img_size - self.patch_size, (1,)).item()
+        y = torch.randint(0, self.img_size - self.patch_size, (1,)).item()
+        img = img[:, x:x+self.patch_size, y:y+self.patch_size]
+        return img
+
+
 
 def prepare_ImageDataset(img_dir, 
                          batch_size,
                          validation_split,
+                         img_size=256,
                          transform=None,
                          seed=123, 
-                         fraction=1.0):
-    dataset = ImageDataset(img_dir, transform, fraction)
+                         fraction=1.0,
+                         subpatch=False,
+                         subpatch_size=128):
+    if subpatch == False:
+        dataset = ImageDataset(img_dir, transform, fraction)
+    else:
+        dataset = SubpatchImageDataset(img_dir, 
+                                       img_size, 
+                                       subpatch_size, 
+                                       seed, 
+                                       transform, 
+                                       fraction)    
     val_len = int(len(dataset) * validation_split)
     train_len = len(dataset) - val_len
     generator = torch.Generator().manual_seed(seed)
